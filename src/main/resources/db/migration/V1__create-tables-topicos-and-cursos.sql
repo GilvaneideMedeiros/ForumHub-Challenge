c57d37e182b10d57f37f2cc6cdb85d99ecdb2050
Gilvaneide Medeiros <gilvaneide.m.bertaccini@gmail.com>
2025-08-12 23:45:49 -0300

    chore: Adiciona script de migração inicial do banco de dados
    Cria o primeiro script de migração do Flyway (V1) para definir a estrutura das tabelas 'cursos' e 'topicos' no banco de dados.

---
+++ src/main/resources/db/migration/V1__create-tables-topicos-and-cursos.sql
@@ -0,0 +1,19 @@
+-- Cria a tabela de cursos
+CREATE TABLE cursos (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+-- Cria a tabela de topicos
+CREATE TABLE topicos (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    titulo VARCHAR(100) NOT NULL,
+    mensagem TEXT NOT NULL,
+    data_criacao DATETIME NOT NULL,
+    estado_topico VARCHAR(50) NOT NULL,
+    autor VARCHAR(100) NOT NULL,
+    curso_id BIGINT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (curso_id) REFERENCES cursos(id)
+);
